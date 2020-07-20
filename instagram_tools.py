ed05a6a213b8e2c5d2150bc6d7686ff1fa258c79
Qurquhart <quintonurquhart@gmail.com>
2020-07-20 08:49:35 -0700

    headless chromedriver/path definition

--- instagram_tools.py
+++ instagram_tools.py
@@ -13,76 +13,75 @@ from selenium.webdriver.common.keys import Keys
 from mountain_project_tools import route_info_by_id
 
 
-def insta_post(ig_username, ig_password, filepath, post_description):
+def insta_post(ig_username, ig_password, filepath, post_description, path_to_chromedriver):
     '''post to instagram using selenium/chrome'''
-    run = False
 
-    if run is False:
-        def loading_time():
-            '''wait for the page to load'''
-            time.sleep(2)
+    def loading_time():
+        '''wait for the page to load'''
+        time.sleep(2)
 
-        # set chrome settings to emulate mobile device
-        mobile_emulation = {
-            "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-            "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D)"
-            " AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
-        chrome_options = Options()
-        chrome_options.add_experimental_option(
-            "mobileEmulation", mobile_emulation)
+    # set chrome settings to emulate mobile device
+    mobile_emulation = {
+        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
+        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D)"
+        " AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_experimental_option(
+        "mobileEmulation", mobile_emulation)
 
-        driver = webdriver.Chrome(
-            'chromedriver.exe', chrome_options=chrome_options)
+    driver = webdriver.Chrome(
+        path_to_chromedriver, chrome_options=chrome_options)
 
-        # navigate to login page and input credentials
-        driver.get('https://www.instagram.com/accounts/login/')
+    # navigate to login page and input credentials
+    driver.get('https://www.instagram.com/accounts/login/')
 
-        loading_time()
+    loading_time()
 
-        driver.find_element_by_name("username").send_keys(ig_username)
-        driver.find_element_by_name("password").send_keys(ig_password)
+    driver.find_element_by_name("username").send_keys(ig_username)
+    driver.find_element_by_name("password").send_keys(ig_password)
 
-        # login button
-        driver.find_element_by_xpath(
-            """//*[@id="react-root"]/section/main/article/div/div/div/form/div[7]/button""").click()
+    # login button
+    driver.find_element_by_xpath(
+        """//*[@id="react-root"]/section/main/article/div/div/div/form/div[7]/button""").click()
 
-        loading_time()
+    loading_time()
 
-        # go to profile page
-        driver.get('https://www.instagram.com/' + ig_username)
+    # go to profile page
+    driver.get('https://www.instagram.com/' + ig_username)
 
-        # init new post
-        ActionChains(driver).move_to_element(driver.find_element_by_xpath(
-            """//*[@id="react-root"]/section/nav[2]/div/div/div[2]/div/div/div[3]""")).click().perform()
+    # init new post
+    ActionChains(driver).move_to_element(driver.find_element_by_xpath(
+        """//*[@id="react-root"]/section/nav[2]/div/div/div[2]/div/div/div[3]""")).click().perform()
 
-        # set filelocation for image upload and open
-        handle = "[CLASS:#32770; TITLE:Open]"
-        autoit.win_wait(handle, 3)
-        autoit.control_set_text(handle, "Edit1", filepath)
-        autoit.control_click(handle, "Button1")
+    # set filelocation for image upload and open
+    handle = "[CLASS:#32770; TITLE:Open]"
+    autoit.win_wait(handle, 3)
+    autoit.control_set_text(handle, "Edit1", filepath)
+    autoit.control_click(handle, "Button1")
 
-        # logging
-        print(f"Uploading {filepath}")
+    # logging
+    print(f"Uploading {filepath}")
 
-        loading_time()
+    loading_time()
 
-        # continue to description options
-        driver.find_element_by_xpath(
-            """//*[@id="react-root"]/section/div[1]/header/div/div[2]/button""").click()
+    # continue to description options
+    driver.find_element_by_xpath(
+        """//*[@id="react-root"]/section/div[1]/header/div/div[2]/button""").click()
 
-        loading_time()
+    loading_time()
 
-        # set description and submit post
-        txt = driver.find_element_by_class_name('_472V_')
-        txt.send_keys('')
-        txt = driver.find_element_by_class_name('_472V_')
-        txt.send_keys(post_description)  # Descrition
-        txt.send_keys(Keys.ENTER)
+    # set description and submit post
+    txt = driver.find_element_by_class_name('_472V_')
+    txt.send_keys('')
+    txt = driver.find_element_by_class_name('_472V_')
+    txt.send_keys(post_description)  # Descrition
+    txt.send_keys(Keys.ENTER)
 
-        # submit button
-        driver.find_element_by_xpath(
-            """//*[@id="react-root"]/section/div[1]/header/div/div[2]/button""").click()
-    run = True
+    # submit button
+    driver.find_element_by_xpath(
+        """//*[@id="react-root"]/section/div[1]/header/div/div[2]/button""").click()
+
 
 
 def mountain_project_poster(ig_username, ig_password, hashtags):
@@ -104,39 +103,42 @@ def mountain_project_poster(ig_username, ig_password, hashtags):
 
     skipped_routes = 0
 
-    for route in route_info["routes"]:
-        # clean this up
-        route_id = route["id"]
-        if route_id not in route_history:
+    posted = False
 
-            images_not_found = 0
-            if not os.path.isfile(f"images/{route_id}.jpg"):
-                images_not_found += 1
+    if posted is False:
+        for route in route_info["routes"]:
+            # clean this up
+            route_id = route["id"]
+            if route_id not in route_history:
+
+                images_not_found = 0
+                if not os.path.isfile(f"images/{route_id}.jpg"):
+                    images_not_found += 1
+                else:
+                    # upload to instagram and add id to route history
+                    try:
+
+                        image = os.path.abspath(f'images/{route_id}.jpg')
+                        instagram_caption = f'{route_info_by_id(route_id)}' + \
+                            '\r\n\r\n\r\n' + hashtags
+                        insta_post(ig_username, ig_password,
+                                image, instagram_caption, 'chromedriver.exe')
+                        route_history.append(route_id)
+                        posted = True
+                        print(instagram_caption)
+
+                    except Exception as ex:
+                        f = open('post_log.txt', 'a+')
+                        f.write(
+                            f'{datetime.now()} - ERROR! UNABLE TO POST ROUTE ID {route_id}: {ex}\r\n')
+                        f.close()
+                        continue
+
+                    # save post history
+                    pickle.dump(route_history, open("data/post_history.p", "wb"))
+
             else:
-                # upload to instagram and add id to route history
-
-                # try:
-
-                image = os.path.abspath(f'images/{route_id}.jpg')
-                instagram_caption = f'{route_info_by_id(route_id)}' + \
-                    '\r\n\r\n\r\n' + hashtags
-                insta_post(ig_username, ig_password,
-                           image, instagram_caption)
-                route_history.append(route_id)
-                print(instagram_caption)
-
-                # except Exception as ex:
-                #    f = open('post_log.txt', 'a+')
-                #    f.write(
-                #        f'{datetime.now()} - ERROR! UNABLE TO POST ROUTE ID {route_id}: {ex}\r\n')
-                #    f.close()
-                #    continue
-
-                # save post history
-                pickle.dump(route_history, open("data/post_history.p", "wb"))
-
-        else:
-            skipped_routes += 1
+                skipped_routes += 1
 
     if images_not_found != 0:
         print(f'{images_not_found} attempted image files were not found.')
